dd04db8fdbe1b15342f391e9d9568ffd2dbf27b5
Artem.Dvornichenko <artemdwo@gmail.com>
2018-08-08 15:25:52 +0100

    [UPDATE] Add state to the App component (=> the username) and pass the username to the UserOutput component

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ import UserOutput from './UserOutput/UserOutput'
 
 // Root component
 class App extends Component {
+
+  state = {
+    users: [
+      {name:"John"},
+      {name:"Jake"},
+      {name:"Jane"},
+      {name:"Joanne"}
+    ]
+  }
+
   render () {
     return (
       <div className="App">
         <h1>A User I\O React application.</h1>
         <hr/>
         <UserInput/>
-        <UserOutput userName='John'/>
-        <UserOutput userName='Jake'/>
-        <UserOutput userName='Jane'/>
-        <UserOutput userName='Joanne'/>
+        <UserOutput userName={this.state.users[0].name}/>
+        <UserOutput userName={this.state.users[1].name}/>
+        <UserOutput userName={this.state.users[2].name}/>
+        <UserOutput userName={this.state.users[3].name}/>
       </div>
     )
   }
